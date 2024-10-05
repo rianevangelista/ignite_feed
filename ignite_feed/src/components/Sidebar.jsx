0f25bfad2c4778071f7b431ca170a8e42c1f07a7
Rian Evangelista <83549817+rianevangelista@users.noreply.github.com>
2024-10-05 19:27:06 +0000

    sidebar config

--- ignite_feed/src/components/Sidebar.jsx
+++ ignite_feed/src/components/Sidebar.jsx
@@ -1,20 +1,28 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
             <img 
-                className=''
+                className={styles.cover}
                 src="https://images.unsplash.com/photo-1605379399642-870262d3d051?q=80&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
                 alt="" 
             />
             <div className={styles.profile}>
+                <img 
+                    className={styles.avatar}
+                    src='https://github.com/rianevangelista.png' 
+                />
+
                 <strong>Rian Evangelista</strong>
                 <span>Desenvolvedor de Software</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>
