eb894185cb106ef06aa804410d6ee02b49d77bc8
Rian Evangelista <83549817+rianevangelista@users.noreply.github.com>
2024-10-18 16:31:11 +0000

    comment box

---
+++ ignite_feed/src/components/Comment.jsx
@@ -0,0 +1,31 @@
+import styles from './Comment.module.css';
+import { Trash, ThumbsUp } from 'phosphor-react';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/rianevangelista.png" alt="" />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Rian Evangelista</strong>
+                            <time title='10 de Outubro às 15:30' dateTime='2024-10-06 15:30:00'>Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={20} />
+                        </button>
+                    </header>
+                    <p>Muito bom! Patabéns!</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
