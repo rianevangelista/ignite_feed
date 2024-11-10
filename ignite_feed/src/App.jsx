59c86342680fed282191475774f3b24b4795d37c
Rian Evangelista <83549817+rianevangelista@users.noreply.github.com>
2024-11-10 01:43:55 +0000

    iteracao post

--- ignite_feed/src/App.jsx
+++ ignite_feed/src/App.jsx
@@ -5,6 +5,37 @@ import { Post } from './components/Post';
 import styles from './App.module.css';
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/rianevangelista.png',
+      name: 'Rian Evangelista',
+      role: 'Software Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }  
+    ],
+    publishedAt: new Date('2024-11-03 10:32:48'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/rianevangelista.png',
+      name: 'Rian Oliveira',
+      role: 'UI/UX Designer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }  
+    ],
+    publishedAt: new Date('2024-11-09 12:12:48'),
+  },
+];
+
 export function App() {
 
   return (
@@ -14,7 +45,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>  
