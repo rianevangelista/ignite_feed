3b26c0213701fe12c5c72e801328637948f7bb6f
Rian Evangelista <83549817+rianevangelista@users.noreply.github.com>
2024-12-02 00:02:31 +0000

    likeComment

--- ignite_feed/src/components/Comment.jsx
+++ ignite_feed/src/components/Comment.jsx
@@ -1,12 +1,21 @@
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 import styles from './Comment.module.css';
 import { Trash, ThumbsUp } from 'phosphor-react';
 
 export function Comment({ content, onDeleteComment }) {
+    const [likeCount, setLikeCount] = useState(0);
+
     function handleDeleteComment() {
         console.log('deletar')
 
         onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1
+        });
     }
 
     return (
@@ -33,9 +42,9 @@ export function Comment({ content, onDeleteComment }) {
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={ handleLikeComment }>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
